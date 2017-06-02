c426962c19fa55072c0f42903ae9fb35d837b130
privers6 <dust1197@yahoo.com>
2017-06-02 12:18:17 -0400

    Branch and Project Setup

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,4 +1,4 @@
-package better_systematic_view;
+package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,11 +10,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("review_selection.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
+
 
     public static void main(String[] args) {
         launch(args);
