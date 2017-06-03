b247363d298e127a9fbc0b47cc3b5c2ed721c388
privers6 <dust1197@yahoo.com>
2017-06-03 13:20:51 -0400

    Moved to FXML based coding.
    Review Selection screen is simply styled. Event handling not yet implemented.

---
+++ src/better_systematic_view/ReviewSelectionController.java
@@ -0,0 +1,13 @@
+package better_systematic_view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class ReviewSelectionController {
+
+    @FXML
+    private void handleAddReview(ActionEvent event) {
+        // Add Review Button was pressed.
+
+    }
+}
