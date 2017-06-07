437ac4f67e1f7b23ba01e5ed033c9cca6ed2b376
David Thomson <dsthomson314@gmail.com>
2017-06-07 08:03:28 -0400

    Checkboxes work properly now

--- src/better_systematic_view/ReviewSelection.java
+++ src/better_systematic_view/ReviewSelection.java
@@ -1,9 +1,8 @@
 package better_systematic_view;
 
-import javafx.collections.FXCollections;
-import javafx.fxml.FXML;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -13,9 +12,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.List;
 
-public class ReviewController {
+public class ReviewSelection {
     @FXML private TableView<Review> table;
     @FXML private TextField addName;
     @FXML private TextField addID;
@@ -42,15 +42,13 @@ public class ReviewController {
         docsArray[1] = new Document(new String[] {"Albert Einstein"}, "Space is weird", "2020");
         docsArray[2] = new Document(new String[] {"John Smith"}, "Behavioral Study", "2021");
 
-        for (Document doc : docsArray) {
-            ReviewScreen.docs.put(doc.hashCode(), doc);
-        }
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("review_screen.fxml"));
+        Parent root = loader.load();
+        ReviewScreen controller = loader.getController();
+        controller.setDocuments(Arrays.asList(docsArray));
 
-        Stage stage = (Stage) table.getScene().getWindow();
-
-        Parent root = FXMLLoader.load(getClass().getResource("review_screen.fxml")); //Should point to second screen
         Scene scene = new Scene(root, 800, 600);
-
+        Stage stage = (Stage) table.getScene().getWindow();
         stage.setScene(scene);
     }
 }
