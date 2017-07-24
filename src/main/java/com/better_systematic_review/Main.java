35ba4bca2e9e3f52924af53f95433c07e0956674
Albert Li <arbylee@gmail.com>
2017-07-23 23:31:15 -0400

    refactored everything for gradle

---
+++ src/main/java/com/better_systematic_review/Main.java
@@ -0,0 +1,80 @@
+package com.better_systematic_review;
+
+import com.better_systematic_review.controller.ReviewSelection;
+import com.better_systematic_review.model.Review;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.nio.file.*;
+import java.util.ArrayList;
+
+public class Main extends Application {
+    private static Path applicationPath;
+    private static Path documentPath;
+    private static Path reviewPath;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        applicationPath = Paths.get(getClass().getProtectionDomain().getCodeSource().getLocation().toURI());
+        Path localPath = applicationPath.getParent().getParent().resolve("local");
+        reviewPath = localPath.resolve("reviews");
+        documentPath = localPath.resolve("documents");
+        try {
+            System.out.println(applicationPath);
+            Files.createDirectory(localPath);
+            Files.createDirectory(reviewPath);
+            Files.createDirectory(documentPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ArrayList<Review> reviewList = new ArrayList<>();
+        try {
+            for (File file : new File(reviewPath.toString()).listFiles()) {
+                FileInputStream f_in = new FileInputStream(file);
+                ObjectInputStream obj_in = new ObjectInputStream(f_in);
+                Object obj = obj_in.readObject();
+                System.out.println(obj);
+                if (obj instanceof Review) {
+                    reviewList.add((Review) obj);
+                }
+            }
+
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/review_selection.fxml"));
+            Parent root = loader.load();
+            primaryStage.setTitle("A Better Systematic Review");
+            Scene scene = new Scene(root, 600, 400);
+            primaryStage.setScene(scene);
+            ReviewSelection reviewSelection = loader.getController();
+            reviewSelection.setTable(reviewList);
+            primaryStage.show();
+        }  catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void init() throws Exception {
+    }
+
+    public static Path getApplicationPath() {
+        return applicationPath;
+    }
+
+    public static Path getDocumentPath(){
+        return documentPath;
+    }
+
+    public static Path getReviewPath() {
+        return reviewPath;
+    }
+}
---
+++ src/main/java/com/better_systematic_review/controller/ReviewCriteria.java
@@ -0,0 +1,92 @@
+package com.better_systematic_review.controller;
+
+import com.better_systematic_review.model.Criteria;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
+import javafx.scene.control.cell.CheckBoxTreeTableCell;
+import javafx.scene.control.cell.TextFieldTreeTableCell;
+import javafx.scene.control.cell.TreeItemPropertyValueFactory;
+
+/**
+ * Created by Porter on 7/14/2017.
+ */
+public class ReviewCriteria {
+    @FXML private TreeTableView<Criteria> treeTable;
+    @FXML private TreeTableColumn<Criteria, String> criteriaName;
+    @FXML private TreeTableColumn<Criteria, Boolean> criteriaReq;
+    private static TreeItem<Criteria> root;
+
+    //Will need to change table creation here to support persistence.
+    @FXML
+    public void initialize() {
+        // Create the RootNode
+        if(root == null) {
+            Criteria criteria = new Criteria();
+            criteria.setName("Root");
+            root = new TreeItem<>(criteria);
+        }
+        root.setExpanded(true);
+        treeTable.setRoot(root);
+
+        // Create Columns with Cell Factories
+        criteriaName.setCellValueFactory(new TreeItemPropertyValueFactory<>("name"));
+        criteriaName.setCellFactory(TextFieldTreeTableCell.<Criteria>forTreeTableColumn());
+
+        criteriaReq.setCellValueFactory(new TreeItemPropertyValueFactory<>("required"));
+        criteriaReq.setCellFactory(CheckBoxTreeTableCell.<Criteria>forTreeTableColumn(criteriaReq));
+    }
+
+    @FXML
+    public void addRow() {
+        TreeItem<Criteria> selectedItem;
+        TreeItem<Criteria> item = new TreeItem<>(new Criteria());
+        TreeTableView.TreeTableViewSelectionModel<Criteria> sm = treeTable.getSelectionModel();
+        if (sm.isEmpty()){
+            root.getChildren().add(item);
+            root.setExpanded(true);
+        }
+        else {
+            // Get the selected row index
+            int rowIndex = sm.getSelectedIndex();
+
+            // Get the selected TreeItem
+            selectedItem = sm.getModelItem(rowIndex);
+
+            // Add the new item as children to the selected item
+            selectedItem.getChildren().add(item);
+
+            // Make sure the new item is visible
+            selectedItem.setExpanded(true);
+        }
+        sm.clearSelection(); //Cant manually deselect rows so this fix is required
+    }
+
+    @FXML
+    public void deleteRow() {
+        // Get the selection model
+        TreeTableView.TreeTableViewSelectionModel<Criteria> sm = treeTable.getSelectionModel();
+        if (sm.isEmpty()) {
+            // Indicate a row needs to be selected
+            return;
+        }
+
+        // Get the selected Entry
+        int rowIndex = sm.getSelectedIndex();
+        TreeItem<Criteria> selectedItem = sm.getModelItem(rowIndex);
+        TreeItem<Criteria> parent = selectedItem.getParent();
+
+        if (parent != null) {
+            // Remove the Item
+            parent.getChildren().remove(selectedItem);
+        }
+        sm.clearSelection(); //Cant manually deselect rows so this fix is required
+    }
+
+    @FXML
+    public void saveCriteria() {
+        // Check whether changes were made and store those changes.
+    }
+}
